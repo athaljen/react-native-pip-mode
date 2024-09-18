fa410d1bb3df936c1b9e86c274f2f7d74185e971
athaljen <athaljen@gmail.com>
2024-09-18 19:07:24 +0530

    fix ios and android issues

--- android/src/main/java/com/pipmode/PipModeModule.kt
+++ android/src/main/java/com/pipmode/PipModeModule.kt
@@ -1,5 +1,6 @@
 package com.pipmode
 
+import android.app.Activity
 import android.app.PictureInPictureParams
 import android.os.Build
 import android.util.Rational
@@ -7,69 +8,72 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
-import android.app.Activity
 
 class PipModeModule(reactContext: ReactApplicationContext) :
-  ReactContextBaseJavaModule(reactContext) {
+    ReactContextBaseJavaModule(reactContext) {
 
-  override fun getName(): String {
-    return NAME
-  }
-
-  // Method to enter Picture-in-Picture mode
-  @ReactMethod
-  fun enterPipMode(promise: Promise) {
-    val activity: Activity? = currentActivity
-
-    if (activity == null) {
-      promise.reject("E_NO_ACTIVITY", "No current activity found.")
-      return
+    init {
+        // Initialization if needed
     }
 
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-      val aspectRatio = Rational(16, 9) // Set the desired aspect ratio for PiP mode
-      val pipBuilder = PictureInPictureParams.Builder()
-        .setAspectRatio(aspectRatio)
-
-      activity.enterPictureInPictureMode(pipBuilder.build())
-      promise.resolve("Entered PiP mode successfully.")
-    } else {
-      promise.reject("E_UNSUPPORTED", "PiP mode is not supported on this device.")
-    }
-  }
-
-  // Method to check if the app is in PiP mode
-  @ReactMethod
-  fun isInPipMode(promise: Promise) {
-    val activity: Activity? = currentActivity
-
-    if (activity == null) {
-      promise.reject("E_NO_ACTIVITY", "No current activity found.")
-      return
+    override fun getName(): String {
+        return NAME
     }
 
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-      promise.resolve(activity.isInPictureInPictureMode)
-    } else {
-      promise.reject("E_UNSUPPORTED", "PiP mode is not supported on this device.")
+    // Method to enter Picture-in-Picture mode
+    @ReactMethod
+    fun enterPipMode(promise: Promise) {
+        val activity: Activity? = currentActivity
+
+        if (activity == null) {
+            promise.reject("E_NO_ACTIVITY", "No current activity found.")
+            return
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val aspectRatio = Rational(9, 16) // Set the desired aspect ratio for PiP mode
+            val pipBuilder = PictureInPictureParams.Builder()
+                .setAspectRatio(aspectRatio)
+
+            activity.enterPictureInPictureMode(pipBuilder.build())
+            promise.resolve("Entered PiP mode successfully.")
+        } else {
+            promise.reject("E_UNSUPPORTED", "PiP mode is not supported on this device.")
+        }
     }
-  }
 
-  @ReactMethod
-  fun isPipSupported(promise: Promise) {
-    val pm = reactContext.packageManager
+    // Method to check if the app is in PiP mode
+    @ReactMethod
+    fun isInPipMode(promise: Promise) {
+        val activity: Activity? = currentActivity
 
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-      val pipPackage = "android.software.picture_in_picture"
+        if (activity == null) {
+            promise.reject("E_NO_ACTIVITY", "No current activity found.")
+            return
+        }
 
-      val isSupported = pm.hasSystemFeature(pipPackage)
-      promise.resolve(isSupported)
-    } else {
-      promise.resolve(false)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            promise.resolve(activity.isInPictureInPictureMode)
+        } else {
+            promise.reject("E_UNSUPPORTED", "PiP mode is not supported on this device.")
+        }
     }
-  }
-  
-  companion object {
-    const val NAME = "PipMode"
-  }
+
+    @ReactMethod
+    fun isPipSupported(promise: Promise) {
+        val pm = reactApplicationContext.packageManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val pipPackage = "android.software.picture_in_picture"
+
+            val isSupported = pm.hasSystemFeature(pipPackage)
+            promise.resolve(isSupported)
+        } else {
+            promise.resolve(false)
+        }
+    }
+
+    companion object {
+        const val NAME = "PipMode"
+    }
 }
--- ios/PipMode.swift
+++ ios/PipMode.swift
@@ -1,8 +1,27 @@
+import AVKit
+import React
+
 @objc(PipMode)
 class PipMode: NSObject {
+    var pipController: AVPictureInPictureController?
 
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
-  }
+    @objc
+    func startPiPMode(_ callback: @escaping RCTResponseSenderBlock) {
+        guard let rootViewController = UIApplication.shared.windows.first?.rootViewController,
+              let playerViewController = rootViewController.presentedViewController as? AVPlayerViewController,
+              let player = playerViewController.player else {
+            callback(["Error: No player view controller available."])
+            return
+        }
+
+        if #available(iOS 9.0, *) {
+            let playerLayer = AVPlayerLayer(player: player)
+            let pipController = AVPictureInPictureController(playerLayer: playerLayer)
+            pipController?.startPictureInPicture()
+            self.pipController = pipController
+            callback([NSNull(), "PiP mode started."])
+        } else {
+            callback(["Error: PiP is only supported on iOS 9.0 and later."])
+        }
+    }
 }
